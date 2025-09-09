a007a98c62a59ffbdd9da527537708ab93707e1e
Ankur-Yadav09 <ankur.yadav@neosoftmail.com>
2025-09-10 00:13:29 +0530

    third version of data

--- mycode.py
+++ mycode.py
@@ -15,8 +15,8 @@ df = pd.DataFrame(data)
 # df.loc[len(df.index)] = new_row_loc
 
 # # Removing one more row from df for V3
-new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-df.loc[len(df.index)] = new_row_loc2
+# new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
